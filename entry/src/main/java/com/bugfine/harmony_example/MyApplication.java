636adbc23c59889c317a057bd28c5777f7d22b2c
tommy.huang <huangcongde1989@gmail.com>
2021-06-20 21:36:47 +0800

    add to github

---
+++ entry/src/main/java/com/bugfine/harmony_example/MyApplication.java
@@ -0,0 +1,10 @@
+package com.bugfine.harmony_example;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/ohosTest/java/com/bugfine/harmony_example/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.bugfine.harmony_example;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.bugfine.harmony_example", actualBundleName);
+    }
+}
